fdf8c1d1f93e974f069763aa0757eeda977995b7
Philipp26 <henrybauers@gmail.com>
2019-07-12 15:00:05 +0400

    fix path

--- app/db_connector.py
+++ app/db_connector.py
@@ -6,7 +6,7 @@ import os
 
 class mysql_connector:
     def get_connect(self):
-        path = r"/home/philipp/say1hello/recruit-python/settings/mysql_settings.json"
+        path = os.path.join(os.path.join('', 'settings'), "mysql_settings.json")
         with open(path) as json_settings:
             data =json.load(json_settings)
 
@@ -21,7 +21,3 @@ class mysql_connector:
 
         return connection
 
-    def exec_query(self, query):
-        with closing(self.get_connect()) as connection:
-            with connection.cursor() as cursor:
-                cursor.execute(query)
--- app/json_parser.py
+++ app/json_parser.py
@@ -7,6 +7,7 @@ class json_parser:
         json_string = json.dumps(data, ensure_ascii=False)
         json_string = re.sub("[\",:\[\]\\\\n\{]", "", json_string)
         messages = json_string.strip().split("}")
+        messages = [message for message in messages if message]
         k = 0
         for message in messages:
             messages[k] = message.strip()
